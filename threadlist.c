ec60516ddd70ee7f8eef0bad031c4f1255a63c3c
ejendret <ejendret@calpoly.edu>
2023-10-22 16:09:27 -0700

    Initial commit to branch emory

---
+++ threadlist.c
@@ -0,0 +1,20 @@
+#include "threadlist.h"
+
+ThreadList * initThreadList() {
+    ThreadList * list = (ThreadList*)malloc(sizeof(CircularLinkedList));
+}
+
+thread addThread(ThreadList * list, thread new_thread){
+    threadToAdd = NULL;
+
+    // If data is null, don't add
+    if (data == NULL)
+    {
+        return threadToAdd;
+    }
+
+    if (list->head == NULL)
+    {
+        list->head = 
+    }
+}
